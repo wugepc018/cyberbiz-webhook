5194470c17bf9a514d6a2a83201bd591f97abad5
Carrine <carrine0976@gmail.com>
2026-03-03 21:03:20 +0800

    Add port

--- cyberbiz.py
+++ cyberbiz.py
@@ -2,16 +2,39 @@ from flask import Flask,jsonify,request
 import json
 import os
 import requests
+import hmac
+import hashlib
+
+APP_SECRET=os.getenv("CYBERBIZ_APP_SECRET")
+if not APP_SECRET:
+    raise ValueError("CYBERBIZ_APP_SECRET not set")
+def verify_signature(request):
+    signature = request.headers.get("X-Cyberbiz-Signature")
+    if not signature:
+        return False
+    raw_body = request.data
+
+    computed = hmac.new(
+        APP_SECRET.encode(),
+        raw_body,
+        hashlib.sha256
+    ).hexdigest()
+
+    return hmac.compare_digest(computed, signature)
 
 app = Flask(__name__)
-RSP_BASE_URL="https://web-production-ee85.up.railway.app"
+RSP_BASE_URL="cyberbiz-webhook-production.up.railway.app"
 RSP_SUBSCRIBE_API = f"{RSP_BASE_URL}/openapi/esim/plan/subscribe"
+
+
 @app.route("/")
 def health():
     return "OK", 200
 
 @app.route("/webhook/cyberbiz/order", methods=["POST"])
 def cyberbiz_order():
+    if not verify_signature(request):
+        return "invalid signature", 403
     data = request.get_json(silent=True)
     print(data)  
     return "ok", 200
@@ -33,11 +56,12 @@ def order_esim():
     "rsp_body": response.json()
         }), 200 
     
-    {
-    "planCode": "A-0001",
-    "cid": "89852000010000000001",
-    "qrcodeType": 0,
-    "email": "abc@123.com"
-    }
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+    
+   
   
 
