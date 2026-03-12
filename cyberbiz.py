208aa469295a67a31a53dbf59da3d16f6fd85738
Carrine <carrine0976@gmail.com>
2026-03-12 16:47:19 +0800

    Update cyberbiz.py

--- cyberbiz.py
+++ cyberbiz.py
@@ -339,7 +339,7 @@ CYBERBIZ_USERNAME="ekzL3c-xypTQ8GJfPi5boF2oPz5TE7xCnfwp8tvf0pY"
 
 def close_cyberbiz_order(order_id):
     http_method = "PUT"
-    url_base = "https://api.cyberbiz.co/"
+    url_base = "https://api.cyberbiz.co"
     url_path = f"/v1/orders/{order_id}/update_status"
     url = url_base + url_path
     x_date = time.strftime('%a, %d %b %Y %H:%M:%S GMT', time.gmtime())
